c052b8e067004b11569951c575841302b0309f60
Maddi Jitendra <116104814+Jitendra797@users.noreply.github.com>
2025-03-08 01:02:05 +0530

    test 4

--- app/tests/export-pattern-check.tsx
+++ app/tests/export-pattern-check.tsx
@@ -2,21 +2,5 @@
 export function MyComponent() {
     return <div>Hello</div>
   }
-  
 
-  
-//   function MyComponent() {
-//     return <div>Hello</div>
-//   }
-//   export default MyComponent
-  
-
-//   const MyComponent = () => {
-//     return <div>Hello</div>
-//   }
-//   export { MyComponent } 
-  
-//   function MyComponent() {
-//     return <div>Hello</div>
-//   }
   
